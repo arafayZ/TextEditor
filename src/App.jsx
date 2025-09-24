9130d8e508d61e0dd622d4f118b825e1456816a0
Abdul Rafay <rafay.inv@gmail.com>
2025-09-24 11:00:42 -0700

    new prj

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import Nav from './Components/Nav'
-import Main from './Components/Main'
+import React from "react";
+import Nav from "./Components/Nav";
+import Main from "./Components/Main";
 
 const App = () => {
   return (
     <div>
-      <Nav/>
-      <Main/>
-      <div class="p-3 mb-2 bg-primary text-white"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius obcaecati ad veritatis, autem aspernatur, rem nam impedit, nobis perspiciatis quis aut illum excepturi? Veniam quidem voluptates hic, nulla ex amet. Reiciendis sed officiis esse incidunt mollitia, dicta maiores fuga repellat, illum, possimus ducimus rem quasi iste debitis fugiat ipsa doloribus.</p></div>
+      <Nav />
+      <Main />
     </div>
-  )
-}
+  );
+};
 
-
-export default App
+export default App;
